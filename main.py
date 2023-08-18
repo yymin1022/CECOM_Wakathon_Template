91a1225bb70b3f909a4a72b8a59ca55bcbd6a06a
yymin1022 <yymin1022@gmail.com>
2023-08-19 01:50:18 +0900

    Update main.py : Added Basic Template Code

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello, World!")
+import sys
+
+
+def main(inputMessage):
+    if len(inputMessage) >= 5:
+        print(get_message(inputMessage))
+
+
+def get_message(strMessage):
+    return strMessage.lower()
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1])
