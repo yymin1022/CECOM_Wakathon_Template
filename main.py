baa02ef277f2e14e915d111e0d05b11ccd40310c
dohun0310 <dohun0310@naver.com>
2023-08-25 12:48:24 +0900

    add your

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main(inputMessage):
     print(get_message(inputMessage))
   elif inputMessage.startswith("와 랜덤 "):
     print(messageRandomNumber(inputMessage))
+  elif inputMessage == "와 니가":
+    print(messageYour(inputMessage))
 
 def get_message(strMessage):
   return strMessage.lower()
@@ -17,6 +19,14 @@ def messageRandomNumber(inputMessage):
     return random.randint(1, number)
   except (IndexError, ValueError):
     return "올바르지 않은 입력이에요. '와 랜덤 [숫자]' 형식으로 입력해주세요."
+  
+def messageYour():
+  return random.choice(
+    [
+      "https://youtu.be/SPddvDupQCM?si=lWzujNG5qSbxRO0u",
+      "뭘 할 수 있는데"
+    ]
+  )
 
 if __name__ == "__main__":
   main(sys.argv[1])
